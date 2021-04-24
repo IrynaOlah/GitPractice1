57ac55e2c80316f73f58c3a6a5d99c7ffdc6b61d
IrynaOlah <irynaolah@gmail.com>
2021-04-24 15:57:05 -0500

    added more work after publishing to GitHub

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("Adding GitHub now");
         System.out.println("Practice");
-        // adding final wrap up code
+        // adding final wrap up code :)
     }
 }
